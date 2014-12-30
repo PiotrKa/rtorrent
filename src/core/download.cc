d7309b5af4a769a57aeb91d25f1140877faa4c98
Piotrek <pepe@trosti.pl>
2014-12-30 18:55:12 +0100

    indent tracker status

--- src/core/download.cc
+++ src/core/download.cc
@@ -112,7 +112,7 @@ Download::receive_tracker_msg(std::string msg) {
   if (msg.empty())
     m_message = "";
   else
-    m_message = "Tracker: [" + msg + "]";
+    m_message = "  Tracker: [" + msg + "]";
 }
 
 float
